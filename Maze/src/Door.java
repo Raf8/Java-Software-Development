f5925140c016d6c694483d909e8b7a537d96d0c9
Rafath <rafath.10@gmail.com>
2018-03-05 19:57:56 +0300

    Add files via upload

---
+++ Maze/src/Door.java
@@ -0,0 +1,12 @@
+
+public class Door extends MapSite {
+	public boolean isOpen;
+	public Room room1;
+	public Room room2;
+	
+	public Door() {}
+		
+	@Override
+	public void enter() {}
+	
+}
---
+++ Maze/src/Maze.java
@@ -0,0 +1,11 @@
+
+
+public class Maze {
+	public void addRoom(Room room) {
+		
+	}
+	
+	public Room[] rooms;
+	
+	public Maze() {}
+}
